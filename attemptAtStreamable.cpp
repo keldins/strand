30f4a1af1268ae73964873a55582a951d1226621
Keldin Sergheyev <keldins@gmail.com>
2018-03-28 15:51:33 -0400

    streamable c program

---
+++ attemptAtStreamable.cpp
@@ -0,0 +1,2 @@
+VimCrypt~01!/„28?’¶(*dôÕ¯±\¼Â#¹u¬ð¹Þz«tÎ•NIypª=Öi1—µáÖïêÐ•‘':‡ƒŒ_vàóätùriËàm»ŠsIQ"Å};¶œÀ;EÙªAØC2:Öª	Ze>Ç{MïÙ³Œ%¿ûlHÙU3šrŠ“zÃZ¶ò©6ÃN‰Õ¬? wNŸ7rù¹ 
+²^8Ä˜XÇÝ7z:¾’q¤¹RŸÃ‡<Ídœ0¾qdýï4©.UÂ+žé\£ø”‡òwwS¾±ÂÀ¦äC¨à•ªµ=HœÓþ“g\3"Ö—ÞÜÃÝk´`‡øwƒ2µ?V¤>Ðný„]×­¨
