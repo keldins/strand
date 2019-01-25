eb81f4c9531f5ffedadc7bd53e299da9a5270987
Keldin Sergheyev <keldins@gmail.com>
2019-01-25 13:50:02 -0500

    Purple plots

--- kns_plot_and_save.m
+++ kns_plot_and_save.m
@@ -2,10 +2,30 @@
 % plot data and save so that files can be transferred from cluster to local
 % machine and then viewed
 
-%load("./kns_variable_dump.mat");
+load("./kns_variable_dump.mat");
+all_times = [times.pre_anneal; times.post_anneal00; times.post_anneal01];
+all_Ts = [Ts.pre_anneal; Ts.post_anneal00; Ts.post_anneal01];
+all_alphas = [alphas.pre_anneal; alphas.post_anneal00; alphas.post_anneal01];
 
-plot(times.post_anneal00, Ts.post_anneal00);
-title("Temperature change over time after annealing sample");
+% T_timestamp, which is the time from the heating element,
+% starts at 39752.61500 seconds
+% all_times, which are all the timestamps from the TGS unit,
+% start at 39907 seconds
+% These numbers seem arcane, but they are actually just clock
+% time in seconds.
+% We subtract off 10 hrs = 36000 seconds to make time axis
+% labels less confusing.
+T_timestamp = T_timestamp - 36000;
+all_times = all_times - 36000;
+
+
+fmt1 = "4";
+fmt2 = "4*";
+fmt3 = "4--";
+
+
+plot(times.post_anneal00, Ts.post_anneal00, fmt1);
+title("Temperature change over time after annealing sample", fmt1);
 xlabel("Time (s)");
 ylabel("Temperature (C)");
 print("-dpdflatex", "~/strand/plots/T_over_time_post_anneal00");
@@ -17,7 +37,7 @@ print("-dpng", "~/strand/plots/T_over_time_post_anneal00");
 
 
 
-plot(Ts.post_anneal00, alphas.post_anneal00, "*");
+plot(Ts.post_anneal00, alphas.post_anneal00, fmt2);
 title("Thermal diffusivity as a function of temperature after annealing sample");
 xlabel("Temperature (C)");
 ylabel("Thermal diffusivity");
@@ -30,7 +50,7 @@ print("-dpng", "~/strand/plots/alpha_vs_T_post_anneal00");
 
 
 
-plot(times.post_anneal00, alphas.post_anneal00, "*"); 
+plot(times.post_anneal00, alphas.post_anneal00, fmt2); 
 title("Thermal diffusivity as a function of time after annealing sample");
 xlabel("Time (s)");
 ylabel("Thermal diffusivity");
@@ -42,11 +62,11 @@ print("-dpdf", "~/strand/plots/alpha_vs_time_post_anneal00");
 print("-dpng", "~/strand/plots/alpha_vs_time_post_anneal00");
 
 
-all_times = [times.pre_anneal; times.post_anneal00; times.post_anneal01];
-all_Ts = [Ts.pre_anneal; Ts.post_anneal00; Ts.post_anneal01];
-all_alphas = [alphas.pre_anneal; alphas.post_anneal00; alphas.post_anneal01];
 
-plot(all_times, all_alphas, "*");
+
+
+
+plot(all_times, all_alphas, fmt2);
 title("Thermal diffusivity over time for entire experiment");
 xlabel("Time (s)");
 ylabel("Thermal diffusivity");
@@ -59,7 +79,7 @@ print("-dpng", "~/strand/plots/alphas_vs_time_all_seconds");
 
 
 
-plot(all_times/3600, all_alphas, "*");
+plot(all_times/3600, all_alphas, fmt2);
 title("Thermal diffusivity over time for entire experiment");
 xlabel("Time (hrs)");
 ylabel("Thermal diffusivity");
@@ -71,7 +91,7 @@ print("-dsvg", "~/strand/plots/alphas_vs_time_all_seconds");
 print("-dpng", "~/strand/plots/alphas_vs_time_all_seconds");
 
 
-plot(all_times, all_Ts, "--");
+plot(all_times, all_Ts, fmt3);
 title("Temperature of sample for duration of experiment");
 xlabel("Time (s)");
 ylabel("Temperature (C)");
@@ -84,7 +104,7 @@ print("-dpng", "~/strand/plots/Ts_vs_time_all_seconds");
 
 
 
-plot(all_times/3600, all_Ts, "--");
+plot(all_times/3600, all_Ts, fmt3);
 title("Temperature of sample for duration of experiment");
 xlabel("Time (hrs)");
 ylabel("Temperature (C)");
@@ -97,7 +117,7 @@ print("-dsvg", "~/strand/plots/Ts_vs_time_all_hours");
 
 
 
-plot(all_Ts, all_alphas, "*");
+plot(all_Ts, all_alphas, fmt2);
 title("Thermal diffusivity as a function of temperature for the entire experiment");
 xlabel("Temperature (C)");
 ylabel("Thermal diffusivity");
@@ -107,6 +127,21 @@ print("-depsc", "~/strand/plots/alphas_vs_Ts_all");
 print("-dpdf", "~/strand/plots/alphas_vs_Ts_all");
 print("-dpng", "~/strand/plots/alphas_vs_Ts_all");
 print("-dsvg", "~/strand/plots/alphas_vs_Ts_all");
+
+
+
+plot(T_timestamp, T_out, fmt1);
+title("Temperature of sample over time");
+xlabel("Time (s)");
+ylabel("Temperature (C)");
+print("-dpdflatex", "~/strand/plots/");
+print("-dpsc", "~/strand/plots/alphas_vs_Ts_all");
+print("-depsc", "~/strand/plots/alphas_vs_Ts_all");
+print("-dpdf", "~/strand/plots/alphas_vs_Ts_all");
+print("-dpng", "~/strand/plots/alphas_vs_Ts_all");
+print("-dsvg", "~/strand/plots/alphas_vs_Ts_all");
+
+
 
 %
 %title("");
---
+++ kns_radiation_damage.m
@@ -0,0 +1,24 @@
+% calculate radiation damage
+
+Damage from each nuclide in decay chain.
+Requires
+E - energy of each nuclide's decay
+how much damage that energy causes (Kinchin-Pease)
+assume no damage heals, and no ion channeling
+assume none of the damaging radiation escapes the sample (obviously not true)
+A - activity of each nuclide
+conc - how much of each nuclide there is in the sample
+λ - decay constants for each member of decay chain
+
+assume pure Th-228 at t=0 with no other radioisotopes
+sample made at t=2.5 years
+assume all members of decay chain were incorporated into sample
+
+
+% Start with activity of each species
+% Neglect gammas and betas
+% Input energy of alpha and daughter of each species
+% Calculate damage from alpha, and then calculate damage from daughter
+% Alpha:
+% Imagine PbS bombarded by alphas
+% 
