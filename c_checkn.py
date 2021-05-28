31781bf20d03f26c9848577a04b453708a30648d
Keldin Sergheyev <keldins@gmail.com>
2021-05-27 18:21:06 -0700

    Clean up

--- c_checkn.py
+++
@@ -1,3 +0,0 @@
-VimCrypt~01!/Ì»!69S~aÍ©ÂYö)”Ø§·©U,t¹ˆ» “¹Ê “Ñ×y™ÑÂ2Ø|mÀQž{ÄŽ«â\þ²Ò†è(ŸŽ[*veQìJ¤À6D–wõ¤V-öuãÛ¢w‘>…)‡P¿H¬öæ“dÇo#âì*=/IöXÇœQ/OP]÷D”Á²ª_s(¾és¸0ý‹!„–hkdu‘¾Û“6i3²ÌyR-Ì¯ù¬WßþP2j+JéÃ:YGÀC2þ¢ ÛnBÏ—„›«]«VgB†oNd™0qó6„ÿ¾35÷×:"Ýo%ÑÐs©®ÓR£x‹ýé3uG!ÕùëDÙ‡¾ç	¬BgEÜšhb€"üß—‹–"áššµ{ê~rÒúß–ÂÛ§ãÂÁÌHà¹šà<^ÄND¿ð˜{bûi¼Ç¦É<‚Æ²štìÿýî¨«	oV²Æ™@¹
-	GjÈ¥Ë!sÃm«ýûy_iÆ_uW(2 àRN/˜Ó@<•Ñª†3F%nô‘/
-{#Zæ¯ÄÙ˜?º$&Ì½b‡*®‘é˜®/ŽÀHJ7Í¦Ó­n¹3$¡7åú²œˆIF• ÄA)_jïß¡»Ú#®SÕRŸæ€'h¬ã’Ç&ú£Gú×'fST!Ä˜©Ý_ý6­¬#ÑÆ2Ä>¢b³‚q/B^–y$
--- kns_find_T_from_time.m
+++
@@ -1,5 +0,0 @@
-function T_for_return = kns_find_T_from_time(times, Ts, timestamp)
-    index = find(timestamp >= times);
-    index = index(end);
-    T_for_return = Ts(index);
-end
